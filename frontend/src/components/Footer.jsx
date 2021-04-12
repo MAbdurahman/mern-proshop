0c3a809a1d01233137868b19f8c3714ba2eefeb6
MAbdurahman <mdbdrrhm@gmail.com>
2021-04-12 07:16:23 -0500

    Complete react-bootstrap setup

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default function Footer() {
+   return (
+		<footer>
+			<Container>
+				<Row>
+               <Col className="text-center py-3">
+                  &copy;&nbsp;2021&nbsp;ProShop,Inc.
+               </Col>
+            </Row>
+			</Container>
+		</footer>
+	);
+}
