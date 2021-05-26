46a849537c4d914de9116a56449f6d94bc07d534
MAbdurahman <mdbdrrhm@gmail.com>
2021-05-26 06:08:18 -0500

    Add functionality to receive products from backend

--- frontend/src/components/screens/HomeScreen.jsx
+++ frontend/src/components/screens/HomeScreen.jsx
@@ -1,20 +1,33 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Row } from 'react-bootstrap';
-import products from '../../products';
+import axios from 'axios';
 import Product from '../Product';
 
+
 export default function HomeScreen() {
+	//**************** variables ****************//
+   const [products, setProducts] = useState([]);
+	//**************** functions ****************//
+   useEffect(() => {
+      const fetchProducts = async () => {
+         const { data } = await axios.get('/api/products');
+         setProducts(data);
+
+      }
+      fetchProducts();
+
+   }, []);
 	return (
 		<>
 			<h1>Latest Products</h1>
-         <Row>
-            {products.map(product => (
-               <Col key={product._id} sm='12' md='6' lg='4' xl='3'>
-                  <Product product={product}/>
-               </Col>
-            ))}
-         </Row>
+			<Row>
+				{products.map(product => (
+					<Col key={product._id} sm='12' md='6' lg='4' xl='3'>
+						<Product product={product} />
+					</Col>
+				))}
+			</Row>
 		</>
 	);
 }
