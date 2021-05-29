82ae90a1554491284dc66215acaf26234af378e8
MAbdurahman <mdbdrrhm@gmail.com>
2021-05-29 05:00:09 -0500

    Add Redux State to HomeScreen with useDispatch() and useSelector()

--- frontend/src/components/screens/HomeScreen.jsx
+++ frontend/src/components/screens/HomeScreen.jsx
@@ -1,33 +1,37 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Col, Row } from 'react-bootstrap';
-import axios from 'axios';
-import Product from '../Product';
 
+import Product from '../Product';
+import { listProducts } from './../../actions/productActions';
 
 export default function HomeScreen() {
 	//**************** variables ****************//
-   const [products, setProducts] = useState([]);
+	const dispatch = useDispatch();
+	const productList = useSelector(state => state.productList);
+	const { loading, error, products } = productList;
+
 	//**************** functions ****************//
-   useEffect(() => {
-      const fetchProducts = async () => {
-         const { data } = await axios.get('/api/products');
-         setProducts(data);
+	useEffect(() => {
+		dispatch(listProducts());
+	}, [dispatch]);
 
-      }
-      fetchProducts();
-
-   }, []);
 	return (
 		<>
 			<h1>Latest Products</h1>
-			<Row>
-				{products.map(product => (
-					<Col key={product._id} sm='12' md='6' lg='4' xl='3'>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<h3>{error}</h3>
+			) : (
+				<Row>
+					{products.map(product => (
+						<Col key={product._id} sm='12' md='6' lg='4' xl='3'>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</>
 	);
 }
