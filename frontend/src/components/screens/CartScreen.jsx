0edc33fe4b6568e1a44691583bd1f9bce6c11611
MAbdurahman <mdbdrrhm@gmail.com>
2021-05-29 11:19:28 -0500

    Add select quantity and addToCart method for the button

---
+++ frontend/src/components/screens/CartScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CartScreen() {
+   return (
+      <div>
+         <h2>Cart Screen</h2>
+      </div>
+   )
+}
--- frontend/src/components/screens/ProductScreen.jsx
+++ frontend/src/components/screens/ProductScreen.jsx
@@ -1,17 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import {
+	Row,
+	Col,
+	Image,
+	ListGroup,
+	Card,
+	Button,
+	Form,
+} from 'react-bootstrap';
 import Rating from '../Rating';
 import Loader from '../Loader';
 import Message from '../Message';
 
 import { listProductDetails } from '../../actions/productActions';
 
-export default function ProductScreen({ match }) {
+export default function ProductScreen({ history, match }) {
 	//**************** variables ****************//
-
+	const [quantity, setQuantity] = useState(0);
 	const dispatch = useDispatch();
 	const productDetails = useSelector(state => state.productDetails);
 	const { loading, error, product } = productDetails;
@@ -22,6 +30,11 @@ export default function ProductScreen({ match }) {
 			dispatch(listProductDetails(match.params.id));
 		}
 	}, [dispatch, match]);
+
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?quantity=${quantity}`);
+	};
+
 	return (
 		<>
 			<Link className='btn btn-light dark-outline my-3' to='/'>
@@ -74,11 +87,37 @@ export default function ProductScreen({ match }) {
 										</Col>
 									</Row>
 								</ListGroup.Item>
+
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Quantity</Col>
+											<Col>
+												<Form.Control
+													as='select'
+													value={quantity}
+													onChange={e =>
+														setQuantity(e.target.value)
+													}
+												>
+													{[
+														...Array(product.countInStock).keys(),
+													].map(x => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
 								<ListGroup.Item>
 									<Button
-										className='btn-block'
+										className='btn-block dark-btn'
 										type='button'
 										disabled={product.countInStock === 0}
+										onClick={addToCartHandler}
 									>
 										Add to Cart
 									</Button>
