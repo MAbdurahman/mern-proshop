d8aebc60657c8361fd18065ea325b5503a7c5391
MAbdurahman <mdbdrrhm@gmail.com>
2021-05-30 08:55:04 -0500

    Complete backend User Authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,20 @@
 //**************** imports ****************//
 import express from 'express';
 import {
-	authUser
+	authUser,
+   getUserProfile,
+   registerUser
 } from '../controllers/userController.js';
+import {
+   protect
+} from '../middleware/authMiddleware.js'
 
 //**************** variables ****************//
 const router = express.Router();
 
 //**************** user routes ****************//
+router.route('/').post(registerUser)
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
