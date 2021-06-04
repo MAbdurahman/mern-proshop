faf06d6fc20010db46ebc2786b6a359d248bb0f7
MAbdurahman <mdbdrrhm@gmail.com>
2021-06-04 06:20:00 -0500

    Add backend endpoint for admin to deleteProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -19,16 +19,91 @@ const getProductById = asyncHandler(async (req, res) => {
 
 	if (product) {
 		res.json(product);
-
 	} else {
 		res.status(404);
 		throw new Error('Product Not Found!');
+	}
+});
 
+/*===============================================================
+      (admin) DeleteProduct  => (DELETE)/api/products/:id
+==================================================================*/
+const deleteProduct = asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		await product.remove();
+		res.json({ message: 'Product Removed!' });
+		
+	} else {
+		res.status(404);
+		throw new Error('Product Not Found!');
+	}
+});
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+/*===============================================================
+      (admin) CreateProduct  => (POST)/api/products
+==================================================================*/
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Sample name',
+		price: 0,
+		user: req.user._id,
+		image: '/images/sample.jpg',
+		brand: 'Sample brand',
+		category: 'Sample category',
+		countInStock: 0,
+		numReviews: 0,
+		description: 'Sample description',
+	});
+
+	const createdProduct = await product.save();
+	res.status(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+/*===============================================================
+      (admin) UpdateProduct  => (PUT)/api/products/:id
+==================================================================*/
+const updateProduct = asyncHandler(async (req, res) => {
+	const {
+		name,
+		price,
+		description,
+		image,
+		brand,
+		category,
+		countInStock,
+	} = req.body;
+
+	const product = await Product.findById(req.params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.description = description;
+		product.image = image;
+		product.brand = brand;
+		product.category = category;
+		product.countInStock = countInStock;
+
+		const updatedProduct = await product.save();
+		res.json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error('Product not found');
 	}
 });
 
 export {
-   getProducts,
-   getProductById
-}
-
+	getProducts,
+	getProductById,
+	deleteProduct,
+	createProduct,
+	updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,15 +2,25 @@
 import express from 'express';
 import {
    getProducts,
-   getProductById
+   getProductById,
+   deleteProduct,
+   createProduct,
+   updateProduct
    } from '../controllers/productController.js';
+import { protect, admin } from './../middleware/authMiddleware.js'
 
 //**************** variables ****************//
 const router = express.Router();
 
 //**************** product routes ****************//
 router.route('/').get(getProducts)
+router.route('/').post(protect, admin, createProduct)
+
+
 router.route('/:id').get(getProductById)
+router.route('/:id').delete(protect, admin, deleteProduct)
+router.route('/:id').put(protect, admin, updateProduct)
+
 
 
 export default router;
