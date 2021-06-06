f490d837ac39d1d229432c8a12f4890d4281e5b4
MAbdurahman <mdbdrrhm@gmail.com>
2021-06-06 08:10:13 -0500

    Add endpoints for createProductReview and getTopProducts

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,14 @@
 //**************** imports ****************//
 import express from 'express';
 import {
-   getProducts,
-   getProductById,
-   deleteProduct,
-   createProduct,
-   updateProduct
-   } from '../controllers/productController.js';
+	getProducts,
+	getProductById,
+	deleteProduct,
+	createProduct,
+	updateProduct,
+	createProductReview,
+	getTopProducts
+} from '../controllers/productController.js';
 import { protect, admin } from './../middleware/authMiddleware.js'
 
 //**************** variables ****************//
@@ -16,11 +18,12 @@ const router = express.Router();
 router.route('/').get(getProducts)
 router.route('/').post(protect, admin, createProduct)
 
+router.route('/:id/reviews').post(protect, createProductReview);
+router.get('/top', getTopProducts);
 
 router.route('/:id').get(getProductById)
 router.route('/:id').delete(protect, admin, deleteProduct)
 router.route('/:id').put(protect, admin, updateProduct)
-
 
 
 export default router;
